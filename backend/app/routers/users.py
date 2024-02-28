149454ddf0919053672d392b66d9a95f9b49d514
mssffct <herooftheday82@mail.ru>
2024-02-28 22:49:10 +0300

    chore (backend/app/routers) init users endpoints

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -11,6 +11,18 @@ router = APIRouter(
 )
 
 
-@router.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
+@router.get("/me")
+async def read_me():
+    return {"message": "Hello World"}
+
+@router.get("/{user_id}")
+async def read_user(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@router.post("/")
+async def create_user(user: dict):
+    return {"user": user}
+
+@router.patch("/{user_id}")
+async def patch_user(user: dict):
+    return {"user": user}
